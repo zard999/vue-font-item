b0b05e69ae46105cedca676b966d3eb9852d5f7f
Forever <1874861374@qq.com>
2021-11-26 09:36:07 +0800

    add: views/Home 主页面home组件化完成

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,18 +1,46 @@
 <template>
   <div>
+    <!-- 三级导航 -->
     <TypeNav></TypeNav>
+
+    <!-- 轮播图 -->
     <ListContainer></ListContainer>
+
+    <!-- 今日推荐 -->
+    <Recommend></Recommend>
+
+    <!-- 商品排行 -->
+    <Rank></Rank>
+
+    <!-- 猜你喜欢 -->
+    <Like></Like>
+
+    <!-- 楼层 -->
+    <Floor></Floor>
+
+    <!-- 商标 -->
+    <Brand></Brand>
   </div>
 </template>
 
 <script>
 import TypeNav from "./TypeNav";
 import ListContainer from "./ListContainer";
+import Recommend from "./Recommend";
+import Rank from "./Rank";
+import Like from "./Like";
+import Floor from "./Floor";
+import Brand from "./Brand";
 export default {
   name: "Home",
   components: {
     TypeNav,
     ListContainer,
+    Recommend,
+    Rank,
+    Like,
+    Floor,
+    Brand,
   },
 };
 </script>
